367b38d7aa6306bc94cdfb164dc712313b669029
romulosilvestre <romulo.silvestre@df.estudante.senai.br>
2023-03-09 11:05:58 -0300

    competência 03 - Concluída

---
+++ LearnCodeProfv2.Web/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnCodeProfv2.Web.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Senha { get; set; }
+    }
+}
+
+
+
+
+
+
+
---
+++ LearnCodeProfv2.Web/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(LearnCodeProfv2.Web.Startup))]
+
+namespace LearnCodeProfv2.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                //Rota (endereço, url)
+                LoginPath= new PathString("/Autenticacao/Login")
+            });                        
+        
+        }
+    }
+}
